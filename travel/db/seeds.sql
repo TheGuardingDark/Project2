0467346375d5046b9f678bf2027d898d4c665122
Scout <therearespidersinthisemail@gmail.com>
2019-08-03 13:23:38 -0600

    added model for tripname started pug styling

---
+++ travel/db/seeds.sql
@@ -0,0 +1,11 @@
+
+--    {tripName: 'Boston Trip', tripDates: '9/28-10/03', tripNotes: 'Boston Red Sox game on Sunday with sis. Dinner with Becky and Mohommad', airlineName: 'Southwest Airlines', flightConNum: 'UX0892', flightTime: '3:25pm-7:45pm', lodgeName: 'Hyatt Regency Cambridge', lodgeConNum: '24311908', checkInTime: '3:00pm'},
+--    {tripName: 'Jamestown Trip', tripDates: '10/03-10/07', tripNotes: 'Platform workshop', airlineName: 'Southwest Airlines', flightConNum: 'S9872X', flightTime: '2:25pm-5:45pm', rentalCarCompany: 'Budget Rental', rentalConNum: '3220092', rentalStartTime: '6:00pm', lodgeName: 'Air BnB', lodgeConNum: '121241241', checkInTime: '3:00pm'},
+--    {tripName: 'OKC Trip', tripDates: '10/14-10/18', tripNotes: 'Family Reunion', airlineName: 'Southwest Airlines', flightConNum: 'T8391N', flightTime: '9:05pm-11:15pm', rentalCarCompany: 'Dollar', rentalConNum: '2343019', rentalStartTime: '11:30pm'},
+ 
+
+INSERT INTO tripnames (tripName) VALUES ('Boston Trip');
+INSERT INTO tripnames (tripName) VALUES ('Jamestown Trip');
+INSERT INTO tripnames (tripName) VALUES ('OKC Trip');
+
+
